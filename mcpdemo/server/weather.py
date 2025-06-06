78f06ee3d18c740b9668acfc90d8a9ba847820e2
adithya1012 <seesa01@pfw.edu>
2025-06-06 16:31:21 -0400

    Simple MCP weather server with MCP-inspector as client

---
+++ mcpdemo/server/weather.py
@@ -0,0 +1,61 @@
+from typing import Any
+import httpx
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("weather")
+
+NWS_API_BASE = "https://api.weather.gov"
+USER_AGENT = "weather-app/1.0"
+
+async def make_nws_request(url):
+    """Make request to NWS API with proper error handling"""
+    header = {
+        "User-agent" : USER_AGENT,
+        "Accept" : "application/geo+json"
+    }
+    async with httpx.AsyncClient() as clint:
+        try:
+            response = await clint.get(url, headers=header, timeout=30.0)
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            return None
+
+def format_alert(feature: dict) -> str:
+    """Format an alert feature into a readable string."""
+    props = feature["properties"]
+    return f"""
+    Event:{props.get ('event', 'Unknown')}
+    Area: {props.get ('areaDes', 'Unknown')} 
+    Severity: {props.get( 'severity', 'Unknown')}
+    Description: {props.get('description', 'No description available')} 
+    Instructions: {props.get('instruction', 'No specific instructions provided')}
+    """
+
+@mcp.tool()
+async def get_alerts(state: str) -> str:
+    """Get weather alerts for a US state.
+    Args:
+    state: Two-letter US state code (e.g. CA, NY)
+    """
+    url = f"{NWS_API_BASE}/alerts/active/area/{state}"
+    data = await make_nws_request(url)
+    if not data or "features" not in data:
+        return "Unable to fetch alerts or no alerts found."
+    if not data["features"]:
+        return "No active alerts for this state."
+    alerts = [format_alert(feature) for feature in data[ "features"]]
+    return "\n---\n".join(alerts)
+
+
+@mcp.tool()
+async def get_add(a, b) -> str:
+    """Gives the addition is 2 numbers
+    Args:
+    a: Integer
+    b: Integer
+    """
+    return int(a)+int(b)
+
+if __name__ == "__main__":
+    mcp.run()
